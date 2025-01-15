56f081a54d659aab1d12b8a9f561c07b1cc59dfa
shabgard82 <pedramaliyari5@gmail.com>
2025-01-15 14:40:03 +0330

    for loop and while nested loop practice

--- learn-js/nestedLoop.js
+++ learn-js/nestedLoop.js
@@ -9,19 +9,6 @@
 // *********
 // **********
 
-let x = 10;
-let y;
-while (x >= 1) {
-  j = i;
-  while (y <= 10) {
-    document.write("*");
-    y++;
-    console.log(j);
-  }
-  document.write("<br>");
-  x--;
-}
-
 // **********
 // *********
 // ********
@@ -33,6 +20,29 @@ while (x >= 1) {
 // **
 // *
 
+//while loop
+let x = 10;
+let y;
+while (x >= 1) {
+  y = x;
+  while (y <= 10) {
+    document.write("*");
+    y++;
+    console.log(j);
+  }
+  document.write("<br>");
+  x--;
+}
+
+//for loop
+for (let i = 10; i >= 1; i--) {
+  for (let j = i; j <= 10; j++) {
+    document.write("*");
+  }
+  document.write("<br>");
+}
+
+//while loop
 let i = 10;
 let j;
 while (i >= 1) {
@@ -44,4 +54,12 @@ while (i >= 1) {
 
   document.write("<br>");
   i--;
+}
+
+//for loop
+for (let i = 10; i >= 1; i--) {
+  for (let j = i; j >= 1; j--) {
+    document.write("*");
+  }
+  document.write("<br>");
 }
